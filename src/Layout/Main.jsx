73807195672cbf7148f4ab131952f7c24c3222bf
Zarinjahanshazi <zarinshazi@gmail.com>
2024-05-15 23:36:32 +0600

    many problem solve

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui in accusantium vitae necessitatibus aspernatur recusandae pariatur expedita facere fugiat nisi? Facere ipsum dolor magnam neque vero quidem atque necessitatibus vitae!</p>
+            
         </div>
     );
 };
