86fa24e4e788b179943d41c8074094877fe40207
Zarinjahanshazi <zarinshazi@gmail.com>
2024-05-15 23:34:00 +0600

    my food

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -9,6 +9,8 @@ const Main = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
+
+            <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam maiores obcaecati ipsa iste expedita, dolore perferendis odio modi consectetur praesentium, molestias suscipit quidem officiis repellendus? Exercitationem illum labore cum in.</P>
         </div>
     );
 };
